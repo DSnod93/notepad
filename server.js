d51f9279750710169d494be3e44e92a3e029f7f5
DSnoddy <douglassnoddyjr@gmail.com>
2021-04-28 21:50:25 -0800

    ability to delete and update notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
+const notes = require('./db/db.json')
 const PORT = process.env.PORT || 3001;
 
 // Middleware
@@ -10,18 +11,68 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 // Notes Array Data
-
+const notesList = [];
 
 
 //  Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+//show notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+// show taken notes
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
 });
+
+// show notes ID
+app.get('/api/notes/:id', (req, res) => {
+    let saveNote = JSON.parse(fs.readFileSync('./db/db.json'));
+    res.json(saveNote[Number(req.params.id)]);
+});
+
+
+// show index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// save notes
+app.post('/api/notes', (req, res) => {
+    const saveNote = JSON.parse(fs.readFileSync('./db/db.json'));
+    const addNote = req.body;
+    const noteId = (saveNote.length).toString();
+    addNote.id = noteId;
+    saveNote.push(addNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(saveNote));
+    res.json(saveNote);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    // ...read all notes from the db.json file
+    let saveNote = JSON.parse(fs.readFileSync('./db/db.json'));
+    let noteID = req.params.id;
+    let newID = 0;
+
+    // ...remove (filter out) the note with the given id property
+    saveNote = saveNote.filter(selectedNote => {
+        return selectedNote.id != noteID;
+    })
+
+    // ...rewrite the notes to the db.json file
+    for (selectedNote of saveNote) {
+        selectedNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(saveNote));
+    res.json(saveNote);
+})
+
+
 
 // Listener
 app.listen(PORT, () => {
